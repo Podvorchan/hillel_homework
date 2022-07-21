f7e17f13d4ab7176fb7b84e3679d335dffcb794f
ruslan <porubo@ukr.net>
2022-07-21 22:10:43 +0300

    redone

--- src/main/java/com/hillel/podvorchan/homework8/Copy.java
+++ src/main/java/com/hillel/podvorchan/homework8/Copy.java
@@ -4,35 +4,24 @@ import java.util.Arrays;
 
 public class Copy {
     public static void main(String[] args) {
-        sum();
-        System.out.println();
-        int[] source = {1, 2, 3, 4};
-        int[] target = copyArray(source);
-        if (Arrays.equals(source, target)) ;
-        {
-            System.out.println(Arrays.toString(target));
-            System.out.println(Arrays.toString(source));
-        }
-    }private static int[] copyArray(int[] source) {
-        int a = source.length;
-        int[] target = new int[a];
-        for (int i = 0; i < a; i++) {
-            target[i] = source[i];
-        }
-        return target;
-    }static void sum() {
-        int[] source = {1, 2, 3, 4};
         int[] target = {5, 6, 7};
-        int[] sum = new int[source.length + target.length];
+        int[] source = {1, 2, 3, 4};
+        System.out.println("source " + Arrays.toString(source));
+        System.out.println("target " + Arrays.toString(target));
+        target = copyArray(source, target);
+        System.out.println("target " + Arrays.toString(target));
+    }static int[] copyArray(int[] target, int[] source) {
+        int[] resoul = new int[source.length + target.length];
         int count = 0;
         for (int i = 0; i < source.length; i++) {
-            sum[i] = source[i];
+            resoul[i] = source[i];
             count++;
         }
         for (int j = 0; j < target.length; j++) {
-            sum[count++] = target[j];
+            resoul[count++] = target[j];
         }
-        for (int i = 0; i < sum.length; i++)
-            System.out.print(sum[i] + " ");
+        for (int i = 0; i < resoul.length; i++) {
+        }
+        return resoul;
     }
 }
